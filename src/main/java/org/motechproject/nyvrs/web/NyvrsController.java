22a75d60ee1a2ea925fc011d61f67fdb2c08624e
Jakub Kondrat <jkondrat@soldevelo.com>
2014-12-10 17:48:46 +0100

    Adding Verboice manifest

--- src/main/java/org/motechproject/nyvrs/web/NyvrsController.java
+++ src/main/java/org/motechproject/nyvrs/web/NyvrsController.java
@@ -1,25 +1,51 @@
 package org.motechproject.nyvrs.web;
 
+import org.apache.commons.io.IOUtils;
 import org.motechproject.nyvrs.service.NyvrsService;
+import org.motechproject.server.config.SettingsFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Controller for NYVRS
  */
 @Controller
+@RequestMapping("/web-api")
 public class NyvrsController {
+
+    @Autowired
+    private SettingsFacade settingsFacade;
 
     @Autowired
     private NyvrsService nyvrsService;
 
     private static final String OK = "OK";
 
-    @RequestMapping("/web-api/status")
+    @RequestMapping("/status")
     @ResponseBody
     public String status() {
         return OK;
     }
+
+    @RequestMapping("/manifest")
+    @ResponseBody
+    public String manifest(HttpServletRequest request) throws IOException {
+
+        String manifestFileName = settingsFacade
+                .getProperty("verboice.manifest.file");
+        InputStream manifest = settingsFacade.getRawConfig(manifestFileName);
+        String manifestBody = IOUtils.toString(manifest);
+
+        String callbackBaseUrl = settingsFacade.getProperty("motech.base.url")
+                + settingsFacade.getProperty("verboice.callback.path");
+
+        return manifestBody.replace("URL_BASE", callbackBaseUrl);
+    }
+
 }
