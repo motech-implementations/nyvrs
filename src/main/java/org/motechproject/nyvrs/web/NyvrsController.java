924d5947c7adf8ea1b00f46c072f55403d766145
Krzysztof Kaczmarczyk <kkaczmarczyk@soldevelo.com>
2014-12-15 14:11:08 +0100

    added isRegistered controller method

--- src/main/java/org/motechproject/nyvrs/web/NyvrsController.java
+++ src/main/java/org/motechproject/nyvrs/web/NyvrsController.java
@@ -1,8 +1,11 @@
 package org.motechproject.nyvrs.web;
 
 import org.apache.commons.io.IOUtils;
+import org.motechproject.nyvrs.service.ClientRegistrationService;
 import org.motechproject.nyvrs.service.NyvrsService;
 import org.motechproject.server.config.SettingsFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +23,16 @@ import java.io.InputStream;
 public class NyvrsController {
 
     @Autowired
+    ClientRegistrationService clientRegistrationService;
+
+    @Autowired
     private SettingsFacade settingsFacade;
 
     @Autowired
     private NyvrsService nyvrsService;
 
     private static final String OK = "OK";
+    private static final Logger LOG = LoggerFactory.getLogger(NyvrsController.class);
 
     @RequestMapping("/status")
     @ResponseBody
@@ -46,6 +53,25 @@ public class NyvrsController {
                 + settingsFacade.getProperty("verboice.callback.path");
 
         return manifestBody.replace("URL_BASE", callbackBaseUrl);
+    }
+
+    @RequestMapping(value = "/isRegistered")
+    @ResponseBody
+    public String isRegistered(HttpServletRequest request) throws IOException {
+        String number = request.getParameter("number");
+
+        if (number == null) {
+            LOG.info("The request doesn't contain number attribute");
+            return "The request doesn't contain number attribute";
+        }
+
+        if (clientRegistrationService.findClientRegistrationByNumber(number) != null) {
+            LOG.info("The client with number " + number + " is registered in NYVRS");
+            return "{ isRegistered: true }";
+        } else {
+            LOG.info("The client with number " + number + " is NOT registered in NYVRS");
+            return "{ isRegistered: false }";
+        }
     }
 
 }
